b355f6e3df458edc5bd9a2c988338a4fc72ac402
Rics <richard_szabo@jataka.hu>
2017-03-22 11:56:51 +0100

    Cleaner handling of shouldShowRequestPermissionRationale based on this: http://stackoverflow.com/a/34612503/21047 : We describe _after_ the permissions has been denied.

--- permissionhandler/src/main/java/hu/rics/permissionhandler/PermissionHandler.java
+++ permissionhandler/src/main/java/hu/rics/permissionhandler/PermissionHandler.java
@@ -15,51 +15,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static android.R.id.message;
+
 /**
  * Created by rics on 2017.03.01..
  */
-
+// permission check based on this: https://inthecheesefactory.com/blog/things-you-need-to-know-about-android-m-permission-developer-edition/en
+// but shouldShowRequestPermissionRationale handling is taken from here: http://stackoverflow.com/a/34612503/21047
 public class PermissionHandler {
     final private int REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS = 123;
     private Activity activity;
     private boolean hasRights = false;
     List<String> allPermissions;
-    List<String> permissionsMissing;
 
     public PermissionHandler(Activity activity) {
         this.activity = activity;
     }
 
     public boolean requestPermission(String[] permissionsNeeded) {
-        // permission check (https://inthecheesefactory.com/blog/things-you-need-to-know-about-android-m-permission-developer-edition/en)
         allPermissions = new ArrayList<>(Arrays.asList(permissionsNeeded));
-        permissionsMissing = new ArrayList<>();
 
-        final List<String> permissionsHave = new ArrayList<>();
+        final List<String> permissionsMissing = new ArrayList<>();
         for( String permission : allPermissions ) {
-            if (!addPermission(permissionsHave, permission)) {
+            if (!addPermission(permissionsMissing, permission)) {
                 Log.i(LibraryInfo.TAG,"permissions missing:" + permission);
-                permissionsMissing.add(permission);
             }
         }
-        Log.i(LibraryInfo.TAG,"permissions - current: " + permissionsHave.size() + " missing:" + permissionsMissing.size());
-        if (permissionsHave.size() > 0) {
-            if (permissionsMissing.size() > 0) {
-                // Need Rationale
-                String message = "You need to grant access to " + permissionsMissing.get(0);
-                for (int i = 1; i < permissionsMissing.size(); i++)
-                    message = message + ", " + permissionsMissing.get(i);
-                showMessageOKCancel(message,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                ActivityCompat.requestPermissions(activity,permissionsHave.toArray(new String[permissionsHave.size()]),
-                                        REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
-                            }
-                        });
-                return false;
-            }
-            ActivityCompat.requestPermissions(activity,permissionsHave.toArray(new String[permissionsHave.size()]),
+        if (permissionsMissing.size() > 0) {
+            ActivityCompat.requestPermissions(activity,permissionsMissing.toArray(new String[permissionsMissing.size()]),
                     REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS);
             return false;
         } else {
@@ -71,43 +54,33 @@ public class PermissionHandler {
     private boolean addPermission(List<String> permissionsList, String permission) {
         if (ContextCompat.checkSelfPermission(activity,permission) != PackageManager.PERMISSION_GRANTED) {
             permissionsList.add(permission);
-            // Check for Rationale Option
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(activity,permission)) {
-                return false;
-            }
+            return false;
         }
         return true;
-    }
-
-    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
-        new AlertDialog.Builder(activity)
-                .setMessage(message)
-                .setPositiveButton("OK", okListener)
-                .setNegativeButton("Cancel", null)
-                .create()
-                .show();
     }
 
     public void onRequestPermissionsResult(int requestCode, String[] permissionsRequested, int[] grantResults) {
         switch (requestCode) {
             case REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS:
             {
-                Map<String, Integer> perms = new HashMap<>();
-                // Initial
-                for(String permission : allPermissions) {
-                    perms.put(permission, PackageManager.PERMISSION_GRANTED);
-                }
-                // Fill with results
+                // Check for Rationale Option
+                hasRights = true;
+                boolean shouldShowRequestPermissionRationale = false;
+                String message = "You need to grant access to ";
                 for (int i = 0; i < permissionsRequested.length; i++) {
                     Log.i(LibraryInfo.TAG,"i:" + i + ":" + permissionsRequested[i] + ":" + grantResults[i] );
-                    perms.put(permissionsRequested[i], grantResults[i]);
+                    if( grantResults[i] != PackageManager.PERMISSION_GRANTED ) {
+                        hasRights = false;
+                        if( ActivityCompat.shouldShowRequestPermissionRationale(activity,permissionsRequested[i]) ) {
+                            shouldShowRequestPermissionRationale = true;
+                            message = message + ", " + permissionsRequested[i];
+                        }
+                    }
                 }
-                if( !perms.containsValue(PackageManager.PERMISSION_DENIED) ) {
-                    // All PermissionHandler Granted
-                    hasRights = true;
-                } else {
-                    // Permission Denied
-                    Toast.makeText(activity, "Some Permission is Denied", Toast.LENGTH_SHORT)
+                if( shouldShowRequestPermissionRationale ) {
+                    new AlertDialog.Builder(activity)
+                            .setMessage(message)
+                            .create()
                             .show();
                 }
             }
